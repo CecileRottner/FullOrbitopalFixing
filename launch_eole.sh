5d471893a016801bd07f82ae872570a1c95976f9
Cécile Rottner <cecile.rottner@edf.fr>
2019-10-18 15:44:37 +0200

    modifs scripts eole

--- launch_eole.sh
+++ launch_eole.sh
@@ -46,5 +46,44 @@ done
 
 
 
+for n in 60 ; do
+for T in 48 96; do
+  for sym in 2 3 4; do
+    for id in {1..20}; do
+	
+	for met in 10 11 30 31 40 41 50 51 60 61 70 71; do
+		rm script.sh
+		
+		echo "#!/usr/bin/env bash" >> "\n" >> script.sh
+		echo -n "./mf "  >> script.sh
+		echo -n ${met} >> script.sh 
+		echo -n " "  >> script.sh 
+		echo -n ${dossier} >> script.sh 
+		echo -n " "  >> script.sh 
+		echo -n ${n} >> script.sh 
+		echo -n " "  >> script.sh 
+		echo -n ${T} >> script.sh 
+		echo -n " "  >> script.sh 
+		echo -n 1 >> script.sh 
+		echo -n " "  >> script.sh 
+		echo -n 3 >> script.sh 
+		echo -n " "  >> script.sh 
+		echo -n ${sym} >> script.sh 
+		echo -n " "  >> script.sh 
+		echo -n 0 >> script.sh 
+		echo -n " "  >> script.sh 
+		echo -n 0 >> script.sh 
+		echo -n " "  >> script.sh 
+		echo -n ${id} >> script.sh 
+   
+		chmod +x script.sh
+		
+        sbatch --exclusive -N 1 --time=12:00:00 --wckey=P11J5:APOGENE script.sh
+
+    done
+  done
+done
+done
+done
 
 
---
+++ launch_eole_test.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+rm Results/*
+
+dossier=data/smaller/
+
+
+for n in 20  ; do
+for T in 48 ; do
+  for sym in 2; do
+    for id in {1..1}; do
+	
+	for met in 10 11 ; do
+		rm script.sh
+		
+		echo "#!/usr/bin/env bash" >> "\n" >> script.sh
+		echo -n "./mf "  >> script.sh
+		echo -n ${met} >> script.sh 
+		echo -n " "  >> script.sh 
+		echo -n ${dossier} >> script.sh 
+		echo -n " "  >> script.sh 
+		echo -n ${n} >> script.sh 
+		echo -n " "  >> script.sh 
+		echo -n ${T} >> script.sh 
+		echo -n " "  >> script.sh 
+		echo -n 1 >> script.sh 
+		echo -n " "  >> script.sh 
+		echo -n 3 >> script.sh 
+		echo -n " "  >> script.sh 
+		echo -n ${sym} >> script.sh 
+		echo -n " "  >> script.sh 
+		echo -n 0 >> script.sh 
+		echo -n " "  >> script.sh 
+		echo -n 0 >> script.sh 
+		echo -n " "  >> script.sh 
+		echo -n ${id} >> script.sh 
+   
+		chmod +x script.sh
+		
+        sbatch --exclusive -N 1 --time=12:00:00 --wckey=P11J5:APOGENE script.sh
+
+    done
+  done
+done
+done
+done
+
+

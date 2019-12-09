627c2f6f1be02d5533cab62f65e1c4aa265ff8b3
CECILE ROTTNER <A79375@eofront1.eole.hpc.edf.fr>
2019-12-09 22:44:05 +0100

    new results sans init

--- getSolutions_noramp.sh
+++ getSolutions_noramp.sh
@@ -5,8 +5,8 @@ for n in 60 ; do
 for T in 48 96 ; do
   for sym in 2 3 4; do
     for id in {1..20}; do
-	
-	for met in 100 150 200 502 ; do
+	#100 101 150 200 300 301 400 401 502 503 600 700
+	for met in  100 101 150 151 200 201 300 301 400 401 502 503 600 700; do
 	
 		fichier=ResultsNoRamp/${n}_${T}_1_3_${sym}_0_0_${id}_${met}.txt
 		
--- launch_eole.sh
+++ launch_eole.sh
@@ -9,9 +9,9 @@ dossier=data/Litt_Real/
 for n in 60; do
 for T in 48; do
   for sym in 4 ; do
-    for id in 6 7 8 18; do
+    for id in 11 13 14 15 16 17; do
 	
-	for met in 501 ; do
+	for met in 53 ; do
 		rm script.sh
 		
 		echo "#!/usr/bin/env bash" >> "\n" >> script.sh
